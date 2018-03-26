897e8f877cd26b8cf3429ec974b1fc478eba1559
tenpoly <ohmusicplus@gmail.com>
2018-03-26 18:59:01 +0900

    test
    mm

---
+++ Assets/Script/VariablesAndFunctions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesAndFunctions : MonoBehaviour
+{
+	int myInt = 5;
+
+    void Start()
+    {
+        myInt = MultiplyByTwo(myInt);
+		int myInt2 = MultiplyByTwo(3);
+        Debug.Log(myInt);
+		Debug.Log(myInt2);
+    }
+
+    int MultiplyByTwo (int number)
+    {
+        return number * 2;
+    }
+}
