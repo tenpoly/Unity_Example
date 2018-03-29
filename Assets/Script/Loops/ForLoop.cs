bfc7bb5bff4b38f2acb3312c93966498155b8816
tenpoly <ohmusicplus@gmail.com>
2018-03-29 18:24:37 +0900

    반복문

---
+++ Assets/Script/Loops/ForLoop.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForLoop : MonoBehaviour
+{
+	public GameObject box;
+
+    void Start()
+    {
+		Instantiate(box, new Vector3(3f, 0f, 2f), Quaternion.identity);
+    }
+}
---
+++ Assets/Script/Loops/Loop_Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loop_Test : MonoBehaviour
+{
+    public GameObject box;
+    public int width;
+    public int height;
+
+
+    void Start()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Instantiate(box, new Vector3(x * 1.5f, 0f, y * 1.5f), Quaternion.identity);
+            }
+        }
+    }
+}
